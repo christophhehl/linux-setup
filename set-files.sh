4292dfca89db52026b990835ca374dbd31b8b308
Christoph Hehl <christophhehl363@gmail.com>
2024-03-08 11:57:08 +0100

    setting files is possible

---
+++ set-files.sh
@@ -0,0 +1,24 @@
+echo "Setting files for alacritty:"
+mkdir -pv ~/.config/alacritty 
+cp -rv ./config/alacritty/* ~/.config/alacritty/
+echo "Setting files for bspwm:"
+mkdir -pv ~/.config/bspwm 
+cp -rv ./config/bspwm/* ~/.config/bspwm/
+echo "Setting files for dunst:"
+mkdir -pv ~/.config/dunst 
+cp -rv ./config/dunst/* ~/.config/dunst/
+echo "Setting files for picom:"
+mkdir -pv ~/.config/picom 
+cp -rv ./config/picom/* ~/.config/picom/
+echo "Setting files for polybar:"
+mkdir -pv ~/.config/polybar 
+cp -rv ./config/polybar/* ~/.config/polybar/
+echo "Setting files for rofi:"
+mkdir -pv ~/.config/rofi 
+cp -rv ./config/rofi/* ~/.config/rofi/
+echo "Setting files for sxhkd:"
+mkdir -pv ~/.config/sxhkd 
+cp -rv ./config/sxhkd/* ~/.config/sxhkd/
+echo "Setting files for wallpaper:"
+mkdir -pv ~/.config/wallpaper 
+cp -rv ./config/wallpaper/* ~/.config/wallpaper/
