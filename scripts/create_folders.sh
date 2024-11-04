9adadeb84ad4fe02b5b9797d517dbb1181edc552
christophhehl <christophhehl363@gmail.com>
2024-11-04 14:21:16 +0100

    verbose create_folders

--- scripts/create_folders.sh
+++ scripts/create_folders.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-mkdir -p ~/projects/golang
-mkdir -p ~/projects/java
-mkdir -p ~/projects/misc
-mkdir -p ~/projects/python
-mkdir -p ~/projects/web
+mkdir -pv ~/projects/golang
+mkdir -pv ~/projects/java
+mkdir -pv ~/projects/misc
+mkdir -pv ~/projects/python
+mkdir -pv ~/projects/web
